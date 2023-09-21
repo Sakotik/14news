eef0423c336b17134964d4277dbe50aae197bbf0
Sakotik <a.shishanovskaya@gmail.com>
2023-09-21 11:50:59 +0300

    1

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,34 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.*;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "requests", schema = "public")
 public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "description", nullable = false, length = 255)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "requestor_id")
+    @ToString.Exclude
+    private User requester;
+
+    private LocalDateTime created;
+
+    public ItemRequest(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
 }
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.request.dto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.UserDto;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequestDto {
+
+    private Long id;
+    private String description;
+    private UserDto requester;
+    private LocalDateTime created;
+
+    public ItemRequestDto(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
 }
